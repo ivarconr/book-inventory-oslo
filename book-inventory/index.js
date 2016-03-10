84b846edbf865fb60bc7fc659de3d236943dc52c
Ivar <ivarconr@gmail.com>
2016-03-10 10:38:22 +0100

    dependencies

--- book-inventory/index.js
+++ book-inventory/index.js
@@ -1,1 +1,9 @@
-console.log('yo!');
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.end('hello world');
+});
+
+server.listen(3000, function() {
+  console.log('listen on http://localhost:3000');
+});
