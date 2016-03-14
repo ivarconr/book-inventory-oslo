172614298e026d4c9c51ffb4a55b0b3f069d7ecf
Ivar <ivarconr@gmail.com>
2016-03-14 09:58:30 +0100

    add content-negotitation

--- routes.js
+++ routes.js
@@ -1,29 +1,49 @@
 module.exports = function(stockRepo) {
-    return {
-      findAll: (req, res, next) => {
-          stockRepo.findAll()
-            .then((items) => res.send(items))
-            .catch(next);
-      },
+  return {
+    findAll: (req, res, next) => {
+      stockRepo.findAll()
+        .then((items) => res.send(items))
+        .catch(next);
+    },
 
-      stockUp: (req, res) => {
-        var book = req.body;
-        console.log('isbn:', book.isbn);
+    stockUp: (req, res) => {
+      var book = req.body;
+      console.log('isbn:', book.isbn);
 
-        stockRepo.stockUp(book.isbn, book.count)
-          .then(() => res.send(book));
-      },
+      stockRepo.stockUp(book.isbn, book.count)
+        .then(() => res.send(book));
+    },
 
-      getCount: (req, res, next) => {
-          stockRepo.getCount(req.params.isbn)
-            .then((count) => {
-              if(count === null) {
-                res.status(404).send("not found");
-              } else {
-                res.send({count : count});
+    getCount: (req, res, next) => {
+      stockRepo.getCount(req.params.isbn)
+        .then((count) => {
+          if (count === null) {
+            res.status(404).send("not found");
+          } else {
+            res.format({
+              'text/plain': function() {
+                res.send('count');
+              },
+
+              'text/html': function() {
+                res.send(`<p>${count}</p>`);
+              },
+
+              'application/json': function() {
+                res.send({
+                  count: count
+                });
+              },
+
+              'default': function() {
+                // log the request and respond with 406
+                res.status(406).send('Not Acceptable');
               }
-            })
-            .catch(next);
-      }
-    };
+            });
+
+          }
+        })
+        .catch(next);
+    }
+  };
 };
